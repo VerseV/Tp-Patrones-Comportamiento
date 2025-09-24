dc9c60704e10b6264415b53e5882a8b08a07661f
VickyA7 <vico.ac2507@gmail.com>
2025-09-24 13:22:35 -0300

    Empezando State y Strategy

---
+++ src/Main/Java/State/Inscripcion.java
@@ -0,0 +1,11 @@
+package Main.java.State;
+
+import Main.Java.EntidadesBase.Alumno;
+import Main.Java.EntidadesBase.Curso;
+import Main.java.State.EstadoInscripcion;
+
+public class Inscripcion {
+    private Alumno alumno;
+    private Curso curso;
+    private EstadoInscripcion estado;
+}
---
+++ src/Main/Java/Strategy/CalculoNota.java
@@ -0,0 +1,4 @@
+package Main.java.Strategy;
+
+public interface CalculoNota {
+}
